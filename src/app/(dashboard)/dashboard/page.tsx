6cff90d9f71cde6908440083e33d0c26eda54cbe
markhwinters <nulimitz360@gmail.com>
2024-07-25 06:45:09 -0600

    Implement authentication

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -8,23 +8,6 @@ export const metadata: Metadata = {
   title: "Dashboard",
 };
 
-// Define menu items
-const CommonMenu = [
-  { name: "Profile", href: "/dashboard/profile" },
-  { name: "Settings", href: "/dashboard/settings" },
-];
-
-const UserMenu = [
-  { name: "My Tasks", href: "/dashboard/my-tasks" },
-  { name: "Messages", href: "/dashboard/messages" },
-];
-
-const AdminMenu = [
-  { name: "User Management", href: "/dashboard/user-management" },
-  { name: "Site Statistics", href: "/dashboard/site-stats" },
-  { name: "Content Moderation", href: "/dashboard/content-moderation" },
-];
-
 // Helper component for rendering menus
 const MenuList = ({ items }: { items: { name: string; href: string }[] }) => (
   <ul>
@@ -40,40 +23,11 @@ export default async function DashboardPage() {
   const { user } = await validateRequest()
 
   if (user) {
-    return redirect("/")
+    return redirect("/sign-in")
   }
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>Welcome, {user?.username}!</p>
-      
-      {/* Common navigation for all users */}
-      <nav>
-        <h2>Main Menu</h2>
-        <MenuList items={CommonMenu} />
-      </nav>
-
-      {/* User-specific content */}
-      {user.role === "USER" && (
-        <div>
-          <h2>User Dashboard</h2>
-          <nav>
-            <MenuList items={UserMenu} />
-          </nav>
-          {/* Add more user-specific content here */}
-        </div>
-      )}
-
-      {/* Admin-specific content */}
-      {user.role === "ADMIN" && (
-        <div>
-          <h2>Admin Dashboard</h2>
-          <nav>
-            <MenuList items={AdminMenu} />
-          </nav>
-          {/* Add more admin-specific content here */}
-        </div>
-      )}
     </div>
   );
 }
