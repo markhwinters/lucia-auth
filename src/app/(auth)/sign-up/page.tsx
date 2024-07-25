7b095487df2b831e3e38f87e229f8bbb42d274e1
markhwinters <nulimitz360@gmail.com>
2024-07-25 06:57:24 -0600

    Update page.tsx

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,4 @@
 import SignUpForm from "@/components/forms/SignUpForm";
-import { cookies } from "next/headers";
 import { validateRequest } from "@/lib/lucia";
 import { redirect } from "next/navigation";
 import { Metadata } from "next";
