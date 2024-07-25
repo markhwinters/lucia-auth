d30587ee51ef5c742fd00eadad0a36392e208b93
markhwinters <nulimitz360@gmail.com>
2024-07-25 06:59:18 -0600

    Update page.tsx

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,23 +1,11 @@
 import { cookies } from "next/headers";
 import { validateRequest } from "@/lib/lucia";
 import { redirect } from "next/navigation";
-import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Dashboard",
 };
-
-// Helper component for rendering menus
-const MenuList = ({ items }: { items: { name: string; href: string }[] }) => (
-  <ul>
-    {items.map((item) => (
-      <li key={item.href}>
-        <Link href={item.href}>{item.name}</Link>
-      </li>
-    ))}
-  </ul>
-);
 
 export default async function DashboardPage() {
   const { user } = await validateRequest()
